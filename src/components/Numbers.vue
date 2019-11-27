3f9873117d77e09e7f28256e75ad4a14c5970b3b
qpham96 <quoc.pham@hotmail.com>
2019-11-27 23:48:07 +0000

    Added when hovering over a number that number is not highlighted

--- src/components/Numbers.vue
+++ src/components/Numbers.vue
@@ -1,76 +1,75 @@
 <template>
   <div>
-    <div class="number" :id="'number-'+number" v-for="number in n()" :key="number" @mouseover="hov(number)" @mouseout="reset">
-      {{number}}
-    </div>
+    <div
+      class="number"
+      :id="'number-'+number"
+      v-for="number in n()"
+      :key="number"
+      @mouseover="hov(number)"
+      @mouseout="reset"
+    >{{number}}</div>
   </div>
 </template>
 
 <script>
 export default {
-  data()
-  {
+  data() {
     return {
       limit: this.$parent.limit,
       numbers: []
-    }
+    };
   },
   watch: {
-    ['$parent.limit'](newLimit)
-    {
-      while(newLimit > 0){              // This limits the limiter so the value cannot be negeative.
-      while(newLimit <= 100){           // This limits the limiter so the value cannot be higher than 100.
-      this.limit = newLimit;
+    ["$parent.limit"](newLimit) {
+      while (newLimit > 0) {
+        // This limits the limiter so the value cannot be negeative.
+        while (newLimit <= 100) {
+          // This limits the limiter so the value cannot be higher than 100.
+          this.limit = newLimit;
+        }
+        newLimit = 100;
+        this.limit = newLimit; //if the input value exceed 100 then it will return 100
       }
-      newLimit = 100
-      this.limit = newLimit;            //if the input value exceed 100 then it will return 100
-      }
-      newLimit = 1
-      this.limit = newLimit;            //if the input value is negative it will return 1.
+      newLimit = 1;
+      this.limit = newLimit; //if the input value is negative it will return 1.
     }
   },
   methods: {
-    n()
-    {
+    n() {
       let numbers = [];
-      for(var i = 0; i < this.limit; i++)
-      {
-        numbers = [...numbers, i+1];    //the random numbers had to be between 1-100, so added the +1 to i as it goes by array index
+      for (var i = 0; i < this.limit; i++) {
+        numbers = [...numbers, i + 1]; //the random numbers had to be between 1-100, so added the +1 to i as it goes by array index
       }
       return numbers.sort(() => Math.random() - 0.5);
     },
-    hov(number)
-    {
-      const nums = document.querySelectorAll('.number');
+    hov(number) {
+      const nums = document.querySelectorAll(".number");
 
-      for(let i = 0; i < nums.length; i++)
-      {
+      for (let i = 0; i < nums.length; i++) {
         const num = nums[i].textContent.trim();
-        if(number % num === 0)
-        {
-          nums[i].classList.add('active')
-          console.log('divisor', num)
-        }
+        if (number % num === 0 && number != num) { // added the number != num to remove the hovered number from being highlighted
+            nums[i].classList.add("active");
+            console.log("divisor", num);
+          }
       }
     },
-    reset()
-    {
-      const nums = document.querySelectorAll('.number');
-      nums.forEach(num => num.classList.remove('active'))
+    reset() {
+      const nums = document.querySelectorAll(".number");
+      nums.forEach(num => num.classList.remove("active"));
     }
   }
-}
+};
 </script>
 
 <style scoped>
-	.number {
-		display: inline-block;
-		padding: 5px;
-		background-color: lightgrey;
-		margin: 5px;
-	}
+.number {
+  display: inline-block;
+  padding: 5px;
+  background-color: 197, 171, 171;
+  margin: 5px;
+}
 
-	.active {
-		background-color: red;
-	}
+.active {
+  background-color: red;
+}
 </style>
